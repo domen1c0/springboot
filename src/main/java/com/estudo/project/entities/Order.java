a8060de32033b9dcb1ecde7ece39d6dc1927c0d4
Paolo Domenico <paolodomenico777@gmail.com>
2022-04-13 12:41:40 -0300

    OrderStatus enum

--- src/main/java/com/estudo/project/entities/Order.java
+++ src/main/java/com/estudo/project/entities/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.estudo.project.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 
@@ -28,6 +29,8 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -36,10 +39,11 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -70,6 +74,20 @@ public class Order implements Serializable {
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
+	}
+	
+	
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null ) {
+			this.orderStatus = orderStatus.getCode();
+		}
+		
+		
 	}
 
 	@Override
